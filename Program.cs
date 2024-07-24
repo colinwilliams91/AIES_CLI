cd13139b7504d4ddf1bd606b2a231cc918651d00
Colin Williams <colin.williams.dev@gmail.com>
2024-07-24 00:29:17 -0500

    feat: start accept app args, new up Open AI assistant client, unable to configure model, SEE client.Name?

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using System.ClientModel;
+using OpenAI.Assistants;
+
+
+#region Suppress_Warnings
+#pragma warning disable OPENAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#pragma warning disable CS8604 // Possible null reference argument.
+#endregion
+ApiKeyCredential openAIKey = Environment.GetEnvironmentVariable("OPEN_AI_API_KEY");
+
+var client = new AssistantClient(openAIKey);
+#region Restore_Warnings
+#pragma warning restore CS8604 // Possible null reference argument.
+#pragma warning restore OPENAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#endregion
+
+if (args.Length < 1)
+{
+    WriteLine("Please provide a file path as the first argument or drag and drop on the binary");
+    return;
+}
+
+var assistantCreationOptions = new AssistantCreationOptions();
+
+WriteLine("Hello, World!");
+
+WriteLine(openAIKey);
